d6ac496eb1177f30e3eafa9a1553f025afe8e8c4
IrsyadHalimi <irsyadhalimi08@gmail.com>
2024-06-30 09:06:10 +0700

    mengurangi kode js dari view template

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,1 +1,25 @@
-<h1>Tes 404</h1>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>404</title>
+    <link rel="shortcut icon" href="{{ asset('assets/compiled/svg/favicon.svg') }}" type="image/x-icon">
+  <link rel="stylesheet" href="{{ asset('assets/compiled/css/app.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/compiled/css/error.css') }}">
+</head>
+<body>
+    <script src="{{ asset('assets/static/js/initTheme.js') }}"></script>
+    <div id="error">
+      <div class="error-page container">
+          <div class="col-md-8 col-12 offset-md-2">
+              <div class="text-center">
+                  <h1 class="error-title">404 NOT FOUND</h1>
+                  <p class='fs-5 text-gray-600'>Halaman yang anda tuju tidak dapat ditemukan.</p>
+              </div>
+          </div>
+      </div>
+    </div>
+</body>
+</html>
